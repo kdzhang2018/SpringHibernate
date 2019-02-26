6df03ac6eae5917760b064f5d290d0f2b94acb8c
Kaidi Zhang <kdzhang818@gmail.com>
2019-02-25 20:55:49 -0500

    sec4

---
+++ spring-demo-one/src/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package springdemo;
+
+public class BaseballCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		return "spend 30 minutes";
+	}
+}
---
+++ spring-demo-one/src/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "run 5k";
+	}
+
+}
